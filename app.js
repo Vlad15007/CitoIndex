70726a6264e3f56c7e8c0020bcb577644aee207d
Vlad15007 <60373120+Vlad15007@users.noreply.github.com>
2023-08-12 19:59:57 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+﻿var api = window.chrome.webview.hostObjects.apiwebcontroller;
+
+var recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition ||
+    window.mozSpeechRecognition ||
+    window.msSpeechRecognition)();
+
+let started = false;
+function Speech() {
+    if (started) {
+        return;
+    }
+    started = true; 
+
+    recognition.lang = 'uk-Ua'
+    recognition.maxAlternatives = 1;
+    recognition.interimResults = false;
+    recognition.start();
+    recognition.onresult = function (event) {
+        var speechResult = event.results[event.results.length - 1][0].transcript;
+        api.CallbackSpeech(speechResult);
+    }
+
+    recognition.onspeechend = function () {
+        setTimeout(() => recognition.start(), 1000);
+    }
+}
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        cells: [],
+        form: null,
+        current: null
+    },
+    mounted() {
+        this.form = new bootstrap.Modal(this.$refs.CellInfo);
+        api.LoadCells();
+    },
+    methods: {
+        ShowCellInfo(cell) {
+            this.current = cell;
+            this.form.show();
+            Speech();
+        },
+        HideCellInfo() {
+            this.form.hide();
+        },
+        LoadCells(cells) {
+            this.cells = cells;
+            setInterval(() => {
+                let json = JSON.stringify(this.cells)
+                api.UpdateCells(json);
+            }, 500)
+        },
+        UpdateCells(newCells) {
+            this.cells = newCells;
+        }
+    }
+})
+
+window.chrome.webview.addEventListener('message', event => {
+    let args = event.data;
+
+    if (args.Method == "LoadCells") {
+        app.LoadCells(args.Data)
+    }
+    else if (args.Method == "UpdateCells") {
+        app.UpdateCells(args.Data)
+    }
+});
+
